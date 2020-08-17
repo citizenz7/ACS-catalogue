31202a18010eb0f831d7f6adc2de2e038c2bb4af
citizenz7 <citizenz7@protonmail.com>
2020-08-17 10:50:07 +0200

    index.php mysql 1rst

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+include_once 'config.php';
+$pagetitle = 'Bienvenue sur '.SITENAMELONG.' !';
+include_once 'header.php';
+?>
+
+<?php include_once 'sidebar.php'; ?>
+
+<!-- 5 derniers articles -->
+<?php
+  try {
+		$stmt = $db->query('SELECT * FROM artiste ORDER BY DESC LIMIT 5');
+    while($row = $stmt->fetch()) {
+?>
+
+  <?php
+    echo html($row['nom']) . '<br>';
+    echo html($row['genre']) . '<br>';
+    echo html($row['pays_origine']) . '<br>';
+    echo html($row['biographie']) . '<br>';
+    echo html($row['discographie']) . '<br>';
+    echo html($row['active']) . '<br>';
+    echo html($row['label']) . '<br>';
+    echo html($row['site_web']) . '<br>';
+    echo html($row['image']) . '<br>';
+    echo html($row['youtube']);
+  ?>
+
+  <?php
+    } //while
+
+  } // try
+
+  catch(PDOException $e) {
+    echo $e->getMessage();
+  }
+  ?>
+
+<?php include_once 'header.php'; ?>
