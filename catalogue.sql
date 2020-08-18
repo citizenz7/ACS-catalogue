53f9ebd19c50e78ee9dc00fc3f776f835a8f919d
citizenz7 <citizenz7@protonmail.com>
2020-08-18 11:04:47 +0200

    artiste date

--- catalogue.sql
+++ catalogue.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 17 août 2020 à 11:54
+-- Généré le :  mar. 18 août 2020 à 09:04
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -34,11 +34,13 @@ CREATE TABLE IF NOT EXISTS `artiste` (
   `nom` varchar(255) NOT NULL,
   `genre` varchar(255) NOT NULL,
   `pays_origine` varchar(255) NOT NULL,
+  `presentation` text NOT NULL,
   `biographie` text NOT NULL,
   `discographie` text NOT NULL,
   `active` varchar(255) NOT NULL,
   `label` varchar(255) NOT NULL,
   `site_web` varchar(255) NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
   `image` varchar(255) DEFAULT NULL,
   `youtube` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -48,10 +50,34 @@ CREATE TABLE IF NOT EXISTS `artiste` (
 -- Déchargement des données de la table `artiste`
 --
 
-INSERT INTO `artiste` (`id`, `nom`, `genre`, `pays_origine`, `biographie`, `discographie`, `active`, `label`, `site_web`, `image`, `youtube`) VALUES
-(1, 'The ACS', 'chanson francaise', 'france', 'acs promo 41', 'The ACS', 'oui', 'auto production', 'https://promo-41.codeur.online/', NULL, NULL),
-(2, 'The pas ACS', 'pas chanson francaise', 'france', 'pas acs', '', '', '', '', NULL, NULL),
-(3, 'The pas ACS', 'pas chanson francaise', 'france', 'pas acs', '', '', '', '', NULL, NULL);
+INSERT INTO `artiste` (`id`, `nom`, `genre`, `pays_origine`, `presentation`, `biographie`, `discographie`, `active`, `label`, `site_web`, `date`, `image`, `youtube`) VALUES
+(1, 'The ACS', 'Chanson Francaise', 'france', '', 'acs promo 41', 'The ACS', 'oui', 'auto production', 'https://promo-41.codeur.online/', '2020-07-31 09:31:06', NULL, NULL),
+(2, 'The pas ACS', 'Funk, goove', 'france', '', 'pas acs', '', '', '', 'https://www.google.fr', '2020-08-17 10:05:42', NULL, NULL),
+(3, 'Jinjer', 'metalcore, death metal, metal progressif', 'Ukraine', 'Jinjer est a l\'origine un groupe de groove metal ukrainien forme en 2009. Combinant brutalite, vitesse, groove et melodie, de nos jours Jinjer se presente lui-meme plutet comme un groupe de progressive groove metal. Bien que les musiciens soient de langue maternelle russe, la langue parlee dans l\'est de l\'Ukraine, le chant, tantet guttural, tantet clair, est en anglais (e l\'exception d\'un titre de l\'album Cloud Factory, Желаю значит получу, \"I want it I\'ll get it\", chante en russe).', 'Debuts et Inhale, Do Not Breathe (2009-2012)\r\nBest Ukrainian Metal Band et Cloud Factory (2013-2015)\r\nNapalm Records et King of Everything (2016-2018)\r\nMicro et Macro (2019)', '    Inhale, Do Not Breathe (reedition) (2013) - The Leaders Records\r\n    Cloud Factory (2014) - The Leaders Records\r\n    King of Everything (2016) - Napalm Records\r\n    Cloud Factory (reedition) (2018) - Napalm Records\r\n    Macro (2019) - Napalm Records', '2009-2020', 'Napalm Records', 'http://jinjer-metal.com/', '2020-08-17 11:34:12', 'jinjer.jpg', 'https://www.youtube.com/watch?v=SQNtGoM3FVU');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `membres`
+--
+
+DROP TABLE IF EXISTS `membres`;
+CREATE TABLE IF NOT EXISTS `membres` (
+  `MemberID` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`MemberID`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `membres`
+--
+
+INSERT INTO `membres` (`MemberID`, `username`, `password`, `email`) VALUES
+(1, 'citizenz7', '$2y$10$g13tR5lU7MeRBp.HxvnVPOskDZwcqykkAFpPkw8Z7ldd3uM53VWFW', 'o.prieur@codeur.online'),
+(2, 'oswald', '$2y$10$vtB37xTfiTgw1.UuX.hfPO7fu3a66eY9Fcpnit7fO0waIuMuZ9kR.', 'o.quevillart.codeur.online'),
+(3, 'yacine', '$2y$10$.Q00/LycRT8anGlaqY1KtOFy4XzSzAGr0i1vNdUUayY3NbPIs1uV2', 'y.sbaÃ¯@codeur.online');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
