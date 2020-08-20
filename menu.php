594a9bec926a28d1f9b3ec396fff0b693f7ef264
Oswald Quevillart <o.quevillart@codeur.online>
2020-08-20 16:16:07 +0200

    navbar!

--- menu.php
+++ menu.php
@@ -3,43 +3,29 @@ $liste_id = $db->query('SELECT id FROM artiste')->fetchAll();
 $id_aleatoire = $liste_id[array_rand($liste_id, 1)]['id'];
 
 ?>
-
-<nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: #cce5ff;">
-  <a class="navbar-brand" href="./"><?php echo $SITENAME; ?></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="./">Accueil <span class="sr-only">(current)</span></a>
+<div id="navbar" class="sticky">
+  <div class="col-md-4">
+  </div>
+  <div class="col-md-4 mid-element">
+    <ul class="nav">
+      <li>
+        <a class="nav-link active" href="./">Home</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="artiste.php?id=<?php echo $id_aleatoire; ?>">&Agrave découvrir<span class="sr-only">(current)</span></a>
+      <li>
+        <a class="nav-link" href="./artiste.php?id=<?php echo $id_aleatoire; ?>">&Agrave découvrir</a>
       </li>
-      <li class="nav-item">
+      <li>
         <a class="nav-link" href="contact.php">Contact</a>
       </li>
-      <!-- <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Artistes/Groupes
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Tri par genre</a>
-          <a class="dropdown-item" href="#">Tri par pays origine</a>
-
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="./admin">Admin</a>
-        </div>
-      </li> -->
-      <li class="nav-item">
+      <li>
         <a class="nav-link" href="./admin">Admin</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0" method="Post" action="recherche.php">
+  </div>
+      <div id="search-bar" class="nl-auto nav-item col-md-4 right-element">
+<form class="form-inline navform" method="Post" action="recherche.php">
       <input class="form-control mr-sm-2" type="search" name="requete" placeholder="Rechercher" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
     </form>
-  </div>
-</nav>
+    </div>
+</div>
