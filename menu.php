444c2b139e12a3d7c53e2dbe244c4bc831ab5b26
Oswald Quevillart <o.quevillart@codeur.online>
2020-08-21 11:49:01 +0200

    navbar responsive without mediaqueries

--- menu.php
+++ menu.php
@@ -4,13 +4,13 @@ $id_aleatoire = $liste_id[array_rand($liste_id, 1)]['id'];
 
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand d-flex w-50 mr-auto" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-    <ul class="nav navbar-nav abs-center-x">
+  <div class="navbar-collapse collapse w-100" id="navbarTogglerDemo02">
+    <ul class="navbar-nav w-100 justify-content-center">
       <li class="nav-item">
         <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
       </li>
@@ -25,7 +25,7 @@ $id_aleatoire = $liste_id[array_rand($liste_id, 1)]['id'];
       <a class="nav-link" href="admin/index.php">Admin</a>
     </li>
     </ul>
-    <form class="form-inline ml-auto" method="post" action="recherche.php">
+    <form class="form-inline ml-auto w-100 justify-content-end" method="post" action="recherche.php">
       <input class="form-control mr-sm-2" type="search" name="requete" placeholder="Rechercher"aria-label="Search">
       <button class="btn my-2 my-sm-0 btn-light" type="submit">Search</button>
     </form>
